96f319ff50a6e5339f044ee60780eb2d0d3a6ce6
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-22 18:22:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,8 +12,7 @@ end
 
 def selection(num)
   if num = 1
-    binding.pry
-     "YUM YUM MUNCH MUNCH MUNCH"
+    "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
     binding.pry
      "HAM HAM HAM IN MY TUMMY"
