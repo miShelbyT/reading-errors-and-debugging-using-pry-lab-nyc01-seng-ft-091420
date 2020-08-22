2e1ad7a7d018a8371449b137027f3ce9b6e9a141
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-22 19:38:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -6,7 +6,6 @@ end
 
 
 def state_log(star_date)
-  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
