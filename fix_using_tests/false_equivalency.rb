528e19cb79413c1b594fa4a85db771f5f3c70f1e
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-22 19:24:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-#require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -16,6 +16,8 @@ def selection(num)
   elsif num = 2
     "HAM HAM HAM IN MY TUMMY"
   end
+  binding.pry
+  puts "hello"
 end
 
 def runner
